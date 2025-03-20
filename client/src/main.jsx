7fb7ce5521c243b40ef9878dc561facd4ced0b2b
Saquib Anjum <saquibanjum8969@gmail.com>
2025-03-20 14:13:19 +0530

    Clerk Auth

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import {BrowserRouter} from 'react-router-dom'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+
+// Import your Publishable Key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+}
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
--- client/src/pages/Result.jsx
+++ client/src/pages/Result.jsx
@@ -18,12 +18,12 @@ const Result = () => {
           <div className="flex flex-col">
             <p className="font-semibold text-gray-5=600 mt-2">Background Removed</p>
             <div className="rounded-md border border-gray-300 h-full relative bg-layers overflow-hidden ">
-              {/* <img src={assets.image_wo_bg} alt="" className=" rounded-md border" /> */}
-              <div className="absolute right-1/2 bottom-1/2 transform translate-x-1/2 translate-y-1/2">
+              <img src={assets.image_wo_bg} alt="" className=" rounded-md border" />
+              {/* <div className="absolute right-1/2 bottom-1/2 transform translate-x-1/2 translate-y-1/2">
                 <div className="border-4 border-violet-600 rounded-full h-12 w-12 border-t-transparent animate-spin">
 
                 </div>
-              </div>
+              </div> */}
              
             </div>
 
@@ -31,9 +31,9 @@ const Result = () => {
 
         </div>
         {/* buttons */}
-        <div className="flex justify-center ">
-          <button>Try another image</button>
-          <a href="">Download image</a>
+        <div className="flex justify-center sm:justify-end item-center flex-wrap gap-4 mt-6">
+          <button className="px-8 py-2.5 text-voilet-600 text-sm border border-violet-600 rounded-full hover:scale-105 transition-all duration-400">Try another image</button>
+          <a className="px-8 py-2.5 text-white text-sm bg-gradient-to-r from-violet-600 to-fuchsia-500  rounded-full hover:scale-105 transition-all duration-400" href="">Download image</a>
         </div>
       </div>
 
